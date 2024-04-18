ad0ca61176872381f1157b120a221ebdf3d203cc
Mike Arnold <MICHAEL.ARNOLD.316@OUTLOOK.COM>
2024-04-18 08:03:47 -0400

    working on submitting a form

--- venv/server.py
+++ venv/server.py
@@ -1,37 +1,26 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, request, redirect
 
 app= Flask(__name__)
 app.debug=True
+
+app.route('/submit_form', methods =['POST'])
+def submit_form():
+  if request.method=='POST':
+    data = request.form.to_dict()
+    return redirect('/thankyou.html')
 
 @app.route('/')
 def index():
   return render_template('index.html')
 
+
+
 @app.route('/<string:page_name>')
 def html_page(page_name):
   return render_template(page_name)
 
-# @app.route('/index.html')
-# def indexpage():
-#   return render_template('index.html')
 
 
-# @app.route('/works.html')
-# def works():
-#   return render_template('works.html')
-
-# @app.route('/work.html')
-# def work():
-#   return render_template('work.html')
-
-
-# @app.route('/about.html')
-# def about():
-#   return render_template('about.html')
-
-# @app.route('/contact.html')
-# def contact():
-#   return render_template('contact.html')
 
 
 
