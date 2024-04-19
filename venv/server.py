5b180ee9f2cc284f22e2fd2f1f2d413506a3e8db
Mike Arnold <MICHAEL.ARNOLD.316@OUTLOOK.COM>
2024-04-19 08:14:23 -0400

    fixed my routing issue with post

--- venv/server.py
+++ venv/server.py
@@ -3,19 +3,19 @@ from flask import Flask, render_template, request, redirect
 app= Flask(__name__)
 app.debug=True
 
-app.route('/submit_form', methods =['POST'])
-def submit_form():
+@app.route('/submit_form', methods =['GET','POST'])
+def help():
   if request.method=='POST':
     data = request.form.to_dict()
-    return redirect('/thankyou.html')
-
-@app.route('/')
+    return redirect('thankyou.html')
+  
+@app.route('/', methods =['GET'])
 def index():
   return render_template('index.html')
 
 
 
-@app.route('/<string:page_name>')
+@app.route('/<string:page_name>', methods =['GET'])
 def html_page(page_name):
   return render_template(page_name)
 
